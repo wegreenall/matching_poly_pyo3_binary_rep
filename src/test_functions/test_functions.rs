b9a3486de626b48b4491abb18b0a76e1d6800c92
wegreenall <wjgreenall@gmail.com>
2023-06-01 12:27:32 +0300

    save for travel to england

--- src/test_functions/test_functions.rs
+++ src/test_functions/test_functions.rs
@@ -5,27 +5,27 @@ use std::mem;
 use crate::matching::{get_matching_polies_stable_graph, get_deck, Graph, _calculate_matching_polynomial_binary, calculate_matching_polynomial_pointer};
 use crate::matching_raw::{calculate_matching_polynomial_raw, GraphProperties, GraphData, get_deck as get_raw_deck};
 
-#[pyfunction]
-pub fn binary_test(data: [usize; mem::size_of::<usize>()*8]) -> Result<Vec<Vec<u64>>, std::io::Error> {
-    // set up the graph and the polies vector
-    let graph = data;
-    let graph_size = graph.graph_size();
-    let graph_clone_list = [graph.clone(), graph.clone(), graph.clone(), graph.clone(), graph.clone(), graph.clone(), graph.clone(), graph.clone()];
+//#[pyfunction]
+//pub fn binary_test(data: [usize; mem::size_of::<usize>()*8]) -> Result<Vec<Vec<u64>>, std::io::Error> {
+    //// set up the graph and the polies vector
+    //let graph = data;
+    //let graph_size = graph.graph_size();
+    //let graph_clone_list = [graph.clone(), graph.clone(), graph.clone(), graph.clone(), graph.clone(), graph.clone(), graph.clone(), graph.clone()];
 
-    let mut polies = Vec::<Vec<u64>>::new();
+    //let mut polies = Vec::<Vec<u64>>::new();
 
-    // now get that polynomial!
-    let graph_poly = calculate_matching_polynomial_binary(graph);
-    polies.push(graph_poly[..=graph_size].to_vec());
-    println!("graph poly: {:?}", graph_poly[..=graph_size].to_vec());
+    //// now get that polynomial!
+    //let graph_poly = _calculate_matching_polynomial_binary(graph);
+    //polies.push(graph_poly[..=graph_size].to_vec());
+    //println!("graph poly: {:?}", graph_poly[..=graph_size].to_vec());
 
-    for graph in graph_clone_list {
-        // spawn a thread for each graph in the deck
-        let poly = calculate_matching_polynomial_binary(graph);
-        polies.push(poly[..=graph_size].to_vec());
-    }
-    Ok(polies)
-}
+    //for graph in graph_clone_list {
+        //// spawn a thread for each graph in the deck
+        //let poly = _calculate_matching_polynomial_binary(graph);
+        //polies.push(poly[..=graph_size].to_vec());
+    //}
+    //Ok(polies)
+//}
 
 #[pyfunction]
 pub fn raw_test(data: [usize; mem::size_of::<usize>()*8]) -> Result<Vec<Vec<u64>>, std::io::Error> {
