1979c610d7e1dd11a9bda308eb53b88a1117aa3b
wegreenall <wjgreenall@gmail.com>
2023-10-09 10:29:08 +0300

    Rejigger the python bindings

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 extern crate matching_poly_lib;
 use matching_poly_lib::graph_matching as matching;
-use matching_poly_lib::matching_raw_memory as matching_raw;
 use matching_poly_lib::weighted_graph_matching as weighted_matching;
 use pyo3::prelude::*;
 use std::mem;
@@ -8,37 +7,39 @@ mod test_functions;
 use test_functions::{pointer_multithreaded_test, basic_multithreaded_test};
 
 mod functions;
-use functions::{calculate_matching_polynomial_pointer,
-                calculate_matching_polynomial_pointer_multithreaded,
-                calculate_matching_polynomial_from_binary_representation,
-                calculate_matching_polynomial_from_binary_representation_multithreaded,
-                calculate_matching_polynomial_from_edges,
-                calculate_matching_polynomial_from_adjacency,
-                calculate_weighted_matching_polynomial,
-                raw_calculate_matching_polynomial,
-                raw_calculate_matching_polynomial_multithreaded
+use functions::{adaptive_matchings,
+                pointer_matchings,
+                pointer_matchings_multithreaded,
+                binary_matchings,
+                binary_matchings_multithreaded,
+                edge_matchings,
+                adjacency_matchings,
+                weighted_matchings,
+                weighted_matchings_parallel,
+                weighted_matchings_non_parallel,
+                get_weighted_poly_from_addresses,
+                get_poly_addresses,
+                weighted_matchings_addresses
                };
-
-const MAX_NODES: usize = mem::size_of::<usize>()*8;
 
 #[pymodule]
 fn matching_rs(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_wrapped(wrap_pyfunction!(calculate_matching_polynomial_from_edges))?;
-    m.add_wrapped(wrap_pyfunction!(calculate_matching_polynomial_from_adjacency))?;
-    m.add_wrapped(wrap_pyfunction!(calculate_matching_polynomial_from_binary_representation))?;
-    m.add_wrapped(wrap_pyfunction!(calculate_matching_polynomial_from_binary_representation_multithreaded))?;
-    m.add_wrapped(wrap_pyfunction!(calculate_matching_polynomial_pointer))?;
-    m.add_wrapped(wrap_pyfunction!(calculate_matching_polynomial_pointer_multithreaded))?;
-    m.add_wrapped(wrap_pyfunction!(calculate_weighted_matching_polynomial))?;
-
-    m.add_wrapped(wrap_pyfunction!(raw_calculate_matching_polynomial))?;
-    m.add_wrapped(wrap_pyfunction!(raw_calculate_matching_polynomial_multithreaded))?;
-    Ok(())
-}
-
-#[pymodule]
-fn matching_rs_tests(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_wrapped(wrap_pyfunction!(adaptive_matchings))?;
+    m.add_wrapped(wrap_pyfunction!(edge_matchings))?;
+    m.add_wrapped(wrap_pyfunction!(adjacency_matchings))?;
+    m.add_wrapped(wrap_pyfunction!(binary_matchings))?;
+    m.add_wrapped(wrap_pyfunction!(binary_matchings_multithreaded))?;
+    m.add_wrapped(wrap_pyfunction!(pointer_matchings))?;
+    m.add_wrapped(wrap_pyfunction!(pointer_matchings_multithreaded))?;
     m.add_wrapped(wrap_pyfunction!(basic_multithreaded_test))?;
     m.add_wrapped(wrap_pyfunction!(pointer_multithreaded_test))?;
+
+    m.add_wrapped(wrap_pyfunction!(weighted_matchings))?;
+    m.add_wrapped(wrap_pyfunction!(weighted_matchings_parallel))?;
+    m.add_wrapped(wrap_pyfunction!(weighted_matchings_non_parallel))?;
+    m.add_wrapped(wrap_pyfunction!(weighted_matchings_addresses))?;
+    m.add_wrapped(wrap_pyfunction!(get_weighted_poly_from_addresses))?;
+
+    m.add_wrapped(wrap_pyfunction!(get_poly_addresses))?;
     Ok(())
 }
